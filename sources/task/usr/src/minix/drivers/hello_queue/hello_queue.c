e813cde987d519fae8b088bae3f617cac1cb90bf
Tomasz Domagała <tomasz.domagala.contact@gmail.com>
2021-06-10 22:54:47 +0200

    Add read and write hello_query functions

--- sources/task/usr/src/minix/drivers/hello_queue/hello_queue.c
+++ sources/task/usr/src/minix/drivers/hello_queue/hello_queue.c
@@ -1,18 +1,33 @@
-#include <minix/drivers.h>
+#include "hello_queue.h"
+
 #include <minix/chardriver.h>
+#include <minix/drivers.h>
+#include <minix/ds.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <minix/ds.h>
 
 #define HELLO_MESSAGE "Hello, World!\n"
 
 /*
  * Function prototypes for the hello driver.
  */
-static int hello_open(devminor_t minor, int access, endpoint_t user_endpt);
-static int hello_close(devminor_t minor);
-static ssize_t hello_read(devminor_t minor, u64_t position, endpoint_t endpt,
-    cp_grant_id_t grant, size_t size, int flags, cdev_id_t id);
+static int hq_open(devminor_t minor, int access, endpoint_t user_endpt);
+static int hq_close(devminor_t minor);
+static ssize_t hq_read(devminor_t minor, u64_t position, endpoint_t endpt,
+                       cp_grant_id_t grant, size_t size, int flags,
+                       cdev_id_t id);
+static ssize_t hq_write(devminor_t UNUSED(minor), u64_t position,
+                        endpoint_t endpt, cp_grant_id_t grant, size_t size,
+                        int UNUSED(flags), cdev_id_t UNUSED(id));
+
+// Fills buffer with repeating "xyz" string.
+static void fill_buffer();
+
+// Remove size bytes from the front of the buffer.
+static void buffer_down(u64_t size);
+
+// Make space for size bytes in the buffer.
+static int buffer_up(u64_t size);
 
 /* SEF functions and variables. */
 static void sef_local_startup(void);
@@ -21,79 +36,88 @@ static int sef_cb_lu_state_save(int);
 static int lu_state_restore(void);
 
 /* Entry points to the hello driver. */
-static struct chardriver hello_tab =
-{
-    .cdr_open	= hello_open,
-    .cdr_close	= hello_close,
-    .cdr_read	= hello_read,
+static struct chardriver hello_tab = {
+    .cdr_open = hq_open,
+    .cdr_close = hq_close,
+    .cdr_read = hq_read,
+	.cdr_write = hq_write,
 };
 
-/** State variable to count the number of times the device has been opened.
- * Note that this is not the regular type of open counter: it never decreases.
- */
+// TODO remove this.
 static int open_counter;
 
-static int hello_open(devminor_t UNUSED(minor), int UNUSED(access),
-    endpoint_t UNUSED(user_endpt))
-{
-    printf("hello_open(). Called %d time(s).\n", ++open_counter);
+// Query buffer.
+char *hq_buffer;
+
+size_t hq_capacity;
+size_t hq_size;
+size_t hq_head;
+
+static int hq_open(devminor_t UNUSED(minor), int UNUSED(access),
+                   endpoint_t UNUSED(user_endpt)) {
     return OK;
 }
 
-static int hello_close(devminor_t UNUSED(minor))
-{
-    printf("hello_close()\n");
-    return OK;
-}
+static int hq_close(devminor_t UNUSED(minor)) { return OK; }
 
-static ssize_t hello_read(devminor_t UNUSED(minor), u64_t position,
-    endpoint_t endpt, cp_grant_id_t grant, size_t size, int UNUSED(flags),
-    cdev_id_t UNUSED(id))
-{
+static ssize_t hq_read(devminor_t UNUSED(minor), u64_t UNUSED(position),
+                       endpoint_t endpt, cp_grant_id_t grant, size_t size,
+                       int UNUSED(flags), cdev_id_t UNUSED(id)) {
     u64_t dev_size;
-    char *ptr;
+    char *ptr = hq_buffer + hq_head;
     int ret;
-    char *buf = HELLO_MESSAGE;
 
-    printf("hello_read()\n");
+    if (hq_size == 0) return 0; /* EOF */
 
-    /* This is the total size of our device. */
-    dev_size = (u64_t) strlen(buf);
-
-    /* Check for EOF, and possibly limit the read size. */
-    if (position >= dev_size) return 0;		/* EOF */
-    if (position + size > dev_size)
-        size = (size_t)(dev_size - position);	/* limit size */
+    if (size > hq_size) size = hq_size;
 
     /* Copy the requested part to the caller. */
-    ptr = buf + (size_t)position;
-    if ((ret = sys_safecopyto(endpt, grant, 0, (vir_bytes) ptr, size)) != OK)
+    if ((ret = sys_safecopyto(endpt, grant, 0, (vir_bytes)ptr, size)) != OK)
         return ret;
+
+    buffer_down(size);
 
     /* Return the number of bytes read. */
     return size;
 }
 
+static ssize_t hq_write(devminor_t UNUSED(minor), u64_t position,
+                        endpoint_t endpt, cp_grant_id_t grant, size_t size,
+                        int UNUSED(flags), cdev_id_t UNUSED(id)) {
+    int ret;
+	char *ptr = hq_buffer + hq_head + hq_size;
+
+    if ((ret = buffer_up(size)) != OK) {
+        return ret;
+    }
+
+    if ((ret = sys_safecopyfrom(endpt, grant, 0, (vir_bytes)ptr, size)) != OK) {
+        return ret;
+    }
+	hq_size += size;
+
+    return size;
+}
+
 static int sef_cb_lu_state_save(int UNUSED(state)) {
-/* Save the state. */
+    /* Save the state. */
     ds_publish_u32("open_counter", open_counter, DSF_OVERWRITE);
 
     return OK;
 }
 
 static int lu_state_restore() {
-/* Restore the state. */
+    /* Restore the state. */
     u32_t value;
 
     ds_retrieve_u32("open_counter", &value);
     ds_delete_u32("open_counter");
-    open_counter = (int) value;
+    open_counter = (int)value;
 
     return OK;
 }
 
-static void sef_local_startup()
-{
+static void sef_local_startup() {
     /*
      * Register init callbacks. Use the same function for all event types
      */
@@ -104,7 +128,8 @@ static void sef_local_startup()
     /*
      * Register live update callbacks.
      */
-    /* - Agree to update immediately when LU is requested in a valid state. */
+    /* - Agree to update immediately when LU is requested in a valid state.
+     */
     sef_setcb_lu_prepare(sef_cb_lu_prepare_always_ready);
     /* - Support live update starting from any standard state. */
     sef_setcb_lu_state_isvalid(sef_cb_lu_state_isvalid_standard);
@@ -115,16 +140,22 @@ static void sef_local_startup()
     sef_startup();
 }
 
-static int sef_cb_init(int type, sef_init_info_t *UNUSED(info))
-{
-/* Initialize the hello driver. */
+static int sef_cb_init(int type, sef_init_info_t *UNUSED(info)) {
+    /* Initialize the hello driver. */
     int do_announce_driver = TRUE;
 
     open_counter = 0;
-    switch(type) {
+    switch (type) {
         case SEF_INIT_FRESH:
+            hq_buffer = malloc(DEVICE_SIZE);
+            if (hq_buffer == NULL) return ENOMEM;
+            hq_capacity = DEVICE_SIZE;
+            hq_size = DEVICE_SIZE;
+            hq_head = 0;
+
+            fill_buffer();
             printf("%s", HELLO_MESSAGE);
-        break;
+            break;
 
         case SEF_INIT_LU:
             /* Restore the state. */
@@ -132,11 +163,11 @@ static int sef_cb_init(int type, sef_init_info_t *UNUSED(info))
             do_announce_driver = FALSE;
 
             printf("%sHey, I'm a new version!\n", HELLO_MESSAGE);
-        break;
+            break;
 
         case SEF_INIT_RESTART:
             printf("%sHey, I've just been restarted!\n", HELLO_MESSAGE);
-        break;
+            break;
     }
 
     /* Announce we are up when necessary. */
@@ -148,8 +179,61 @@ static int sef_cb_init(int type, sef_init_info_t *UNUSED(info))
     return OK;
 }
 
-int main(void)
-{
+static void fill_buffer() {
+    char *pattern = "xyz";
+
+    u64_t i = 0, j = 0;
+    for (; i < hq_capacity; i++, j++) {
+        if (j == 3) {
+            j = 0;
+        }
+        hq_buffer[i] = pattern[j];
+    }
+}
+
+static void buffer_down(u64_t size) {
+    hq_head += size;
+    hq_size -= size;
+
+    if (hq_size < hq_capacity / 4) {
+        if (hq_size > 0) {
+            memmove(hq_buffer, hq_buffer + hq_head, hq_size);
+        }
+        hq_head = 0;
+
+        size_t new_capacity = hq_capacity / 2;
+
+        if (new_capacity < 1) return;
+
+        char *ptr = realloc(hq_buffer, new_capacity);
+
+        if (ptr == NULL) return;
+        hq_buffer = ptr;
+        hq_capacity = new_capacity;
+    }
+}
+
+static int buffer_up(u64_t size) {
+    if (hq_head + hq_size + size > hq_capacity && hq_head != 0) {
+        memmove(hq_buffer, hq_buffer + hq_head, hq_size);
+        hq_head = 0;
+    }
+
+    if (hq_size + size > hq_capacity) {
+        size_t new_capacity = hq_capacity;
+        while (new_capacity <= hq_size + size) {
+            new_capacity *= 2;
+        }
+        char *ptr = realloc(hq_buffer, new_capacity);
+        if (ptr == NULL) return ENOMEM;
+		hq_buffer = ptr;
+        hq_capacity = new_capacity;
+    }
+
+    return OK;
+}
+
+int main(void) {
     /*
      * Perform initialization.
      */
@@ -161,4 +245,3 @@ int main(void)
     chardriver_task(&hello_tab);
     return OK;
 }
-
